792a6c6b30e14544eaa815b57da425a698ace98b
Tristanvandermerwe11 <ST10471259@vcconnect.edu.za>
2025-03-04 11:39:53 +0200

    i added some code

--- prjClasses/src/main/java/com/mycompany/prjclasses/PrjClasses.java
+++ prjClasses/src/main/java/com/mycompany/prjclasses/PrjClasses.java
@@ -10,7 +10,9 @@ package com.mycompany.prjclasses;
  */
 public class PrjClasses {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args) 
+    {
+        System.out.println("BBBring back feastables");
+        System.out.println("hello world! Ijust did my first gitHub project and i hate reece now yay");
     }
 }
