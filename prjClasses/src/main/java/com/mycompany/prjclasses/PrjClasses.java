ce295ba9527078b973c30c09f58028a1e530804e
Tristanvandermerwe11 <ST10471259@vcconnect.edu.za>
2025-03-11 10:38:39 +0200

    added code

--- prjClasses/src/main/java/com/mycompany/prjclasses/PrjClasses.java
+++ prjClasses/src/main/java/com/mycompany/prjclasses/PrjClasses.java
@@ -12,7 +12,10 @@ public class PrjClasses {
 
     public static void main(String[] args) 
     {
-        System.out.println("BBBring back feastables");
-        System.out.println("hello world! Ijust did my first gitHub project and i hate reece now yay");
+        newClass nc = new newClass();
+        nc.helloClass();
+        
+        System.out.println(nc.secondClass);
+        
     }
 }
---
+++ prjClasses/src/main/java/com/mycompany/prjclasses/newClass.java
@@ -0,0 +1,20 @@
+package com.mycompany.prjclasses;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author lab_services_student
+ */
+public class newClass 
+{
+    public void helloClass()
+    {
+        System.out.println("hello from the second class");
+        
+    }
+    public String secondClass = "testing";
+}
